ec7b65d60fbe2239eaa25f31b953b57e3c862ced
Shubhanshu Gupta <shubhanshu2007@gmail.com>
2015-11-25 19:08:47 +0530

    Added the configuration engine and mounted the engine

--- RSS/config/routes.rb
+++ RSS/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+mount SuperfeedrEngine::Engine => SuperfeedrEngine::Engine.base_path
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
